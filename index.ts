7be45a72b2f40f1aedd9688584e774919a3ff618
RafiAbqor <rafi.abqor@gmail.com>
2024-07-29 22:11:53 +0700

    inisiasi class controller, service, routes

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import PostRoute from "./src/routes/PostRoute";
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use("/post", PostRoute);
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send("Hello World!, coba2");
