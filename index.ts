ffa92495ce91d678ea1a8d324cac594235bad357
RafiAbqor <rafi.abqor@gmail.com>
2024-09-02 13:49:43 +0700

    coba-ubah-cors

--- index.ts
+++ index.ts
@@ -18,7 +18,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://pctrn.vercel.app',
+    optionsSuccessStatus: 200
+  }));
 
 app.use("/uploads",express.static("src/uploads"));
 
