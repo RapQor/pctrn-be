ac15396ee02b276a008c7b1327f9f5fa4bef3c61
RafiAbqor <rafi.abqor@gmail.com>
2024-09-03 11:04:52 +0700

    update-post

--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -1,5 +1,5 @@
 import { Posts } from "@prisma/client";
 
-export interface IPosts extends Omit<Posts, 'images'> {
+export interface IPosts extends Omit<Posts, 'image'> {
     images?: { image: string }[];
 }
