1dbaf69d8e52b11f531eb0cf2493fdaf12d9674a
RafiAbqor <rafi.abqor@gmail.com>
2024-09-03 11:21:01 +0700

    update-post

--- src/middlewares/fileUpload.ts
+++ src/middlewares/fileUpload.ts
@@ -2,6 +2,11 @@ import multer from "multer";
 
 // Menggunakan memory storage agar file disimpan di memori sebelum diunggah ke Cloudinary
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ 
+    storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024 // 10 MB
+    }
+});
 
 export default upload;
